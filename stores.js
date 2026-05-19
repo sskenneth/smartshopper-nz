81155e78ff34809c87c6b0bb7e7e3b960d2d4c56
sskenneth <sharein1612@gmail.com>
2026-05-19 15:11:45 +1200

    Create stores.js

---
+++ stores.js
@@ -0,0 +1,58 @@
+export const stores = [
+  {
+    name: "Pak'nSave Dunedin",
+    searchUrl: "PASTE_STORE_SEARCH_URL_HERE?q="
+  },
+  {
+    name: "New World Centre City",
+    searchUrl: "PASTE_STORE_SEARCH_URL_HERE?q="
+  },
+  {
+    name: "New World Gardens",
+    searchUrl: "PASTE_STORE_SEARCH_URL_HERE?q="
+  },
+  {
+    name: "The Warehouse",
+    searchUrl: "PASTE_STORE_SEARCH_URL_HERE?q="
+  },
+  {
+    name: "Countdown South Dunedin",
+    searchUrl: "PASTE_STORE_SEARCH_URL_HERE?q="
+  },
+  {
+    name: "Fresh Choice Roslyn",
+    searchUrl: "PASTE_STORE_SEARCH_URL_HERE?q="
+  },
+  {
+    name: "Countdown Dunedin Central",
+    searchUrl: "PASTE_STORE_SEARCH_URL_HERE?q="
+  },
+  {
+    name: "Woolworths Mornington",
+    searchUrl: "PASTE_STORE_SEARCH_URL_HERE?q="
+  },
+  {
+    name: "Fresh Choice Green Island",
+    searchUrl: "PASTE_STORE_SEARCH_URL_HERE?q="
+  },
+  {
+    name: "Four Square Caversham",
+    searchUrl: "PASTE_STORE_SEARCH_URL_HERE?q="
+  },
+  {
+    name: "Four Square Brockville",
+    searchUrl: "PASTE_STORE_SEARCH_URL_HERE?q="
+  },
+  {
+    name: "Foodlands Four Square",
+    searchUrl: "PASTE_STORE_SEARCH_URL_HERE?q="
+  },
+  {
+    name: "New World Mosgiel",
+    searchUrl: "PASTE_STORE_SEARCH_URL_HERE?q="
+  },
+  {
+    name: "Woolworths Mosgiel",
+    searchUrl: "PASTE_STORE_SEARCH_URL_HERE?q="
+  }
+];
